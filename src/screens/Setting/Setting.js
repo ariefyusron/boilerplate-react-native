e3547c3615bd54eb99c810e8187794c6834c8342
ariefyusron <ariefyusron98@gmail.com>
2019-11-30 18:13:05 +0700

    bug fix

---
+++ src/screens/Setting/Setting.js
@@ -0,0 +1,101 @@
+import React, { Component, Fragment } from 'react';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { connect } from 'react-redux';
+
+import I18n from '../../I18n';
+import { chooseLanguage } from '../../redux/actions';
+
+class Setting extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('header') || I18n.t('setting')
+  });
+
+  async handleClick(value) {
+    await this.props.chooseLanguage(value);
+    this.props.navigation.setParams({ header: I18n.t('setting') });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.content} elevation={3}>
+          <TouchableHighlight
+            style={[styles.button, styles.border]}
+            underlayColor="rgba(100,100,100,0.1)"
+            disabled={this.props.setting.language === 'en'}
+            onPress={() => this.handleClick('en')}>
+            <Fragment>
+              <Text style={styles.text}>English</Text>
+              {this.props.setting.language === 'en' ? (
+                <View style={styles.wrapIcon}>
+                  <Icon name="check" size={20} />
+                </View>
+              ) : null}
+            </Fragment>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor="rgba(100,100,100,0.1)"
+            disabled={this.props.setting.language === 'id'}
+            onPress={() => this.handleClick('id')}>
+            <Fragment>
+              <Text style={styles.text}>Bahasa Indonesia</Text>
+              {this.props.setting.language === 'id' ? (
+                <View style={styles.wrapIcon}>
+                  <Icon name="check" size={20} />
+                </View>
+              ) : null}
+            </Fragment>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center'
+  },
+  content: {
+    width: '80%',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    marginTop: 50,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2
+  },
+  button: {
+    paddingVertical: 20,
+    width: '100%',
+    alignItems: 'center'
+  },
+  border: {
+    borderBottomColor: 'rgba(0,0,0,0.2)',
+    borderBottomWidth: 0.5
+  },
+  text: {
+    fontSize: 16
+  },
+  wrapIcon: {
+    position: 'absolute',
+    right: 10,
+    top: 20
+  }
+});
+
+const mapStateToProps = state => ({
+  setting: state.setting
+});
+
+const mapDispatchToProps = {
+  chooseLanguage
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Setting);
